257193987ef9fb867cb0287ba193409c1d22f0f5
RajeevBam <abhibam01@gmail.com>
2024-04-07 21:10:07 -0400

    NavBar Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import Portfolio from "./components/Portfolio/Portfolio";
+import Packages from "./components/Packages/Packages"
+import Cart from "./components/Cart/Cart"
 
 function App() {
   return (
-    <div className="App">
-     <h1>Dynamo Vision</h1>
-
-    </div>
+    <Router>
+<Routes>
+<Route path="/" exact component={Portfolio} />
+      <Route path="/packages" component={Packages} />
+      <Route path="/cart" component={Cart} />
+</Routes>
+  </Router>
   );
 }
 
